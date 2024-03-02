7f4beebfaa6dbc4d4516ec044b0b2c9074aae4ef
BaoNguyen0407 <160560075+BaoNguyen0407@users.noreply.github.com>
2024-03-02 18:47:03 +0700

    Update AccountFragment.java

--- app/src/main/java/com/example/quizapp/AccountFragment.java
+++ app/src/main/java/com/example/quizapp/AccountFragment.java
@@ -25,6 +25,7 @@ import com.google.firebase.auth.FirebaseAuth;
  */
 public class AccountFragment extends Fragment {
 
+    //Bao dang tap code
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
